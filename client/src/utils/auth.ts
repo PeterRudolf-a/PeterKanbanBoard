5f6d2b5312214b54dfd25aca2713d95f3d336980
Peter Rudolf <peterrudolf249@gmail.com>
2025-02-04 20:44:57 -0500

    change to auth.ts to check URL for logout function

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,4 +1,4 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 class AuthService {
   getProfile() {
@@ -15,7 +15,7 @@ class AuthService {
     const loggedIn = this.getToken() ? true : false;
     return loggedIn;
   }
-  
+
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
     const decoded = jwtDecode<JwtPayload>(token);
@@ -28,22 +28,31 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
-    return localStorage.getItem('id_token') || '';
+    return localStorage.getItem("id_token") || "";
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     const token = idToken;
-    localStorage.setItem('id_token', token);
+    localStorage.setItem("id_token", token);
     // TODO: redirect to the home page
-    window.location.assign('/');
+    window.location.assign("/");
   }
 
   logout() {
     // TODO: remove the token from localStorage
-    localStorage.removeItem('id_token');
+    localStorage.removeItem("id_token");
     // TODO: redirect to the login page
-    window.location.assign('/auth/login');
+    // Check if the application is running in production or development
+    const isProduction =
+      window.location.hostname === "peterkanbanboard.onrender.com";
+
+    // Redirect to the appropriate login page based on the environment
+    if (isProduction) {
+      window.location.assign("https://peterkanbanboard.onrender.com/login");
+    } else {
+      window.location.assign("/login"); // Local redirection
+    }
   }
 }
 
